a430f85e3666fea003af920e8bc26d25c3dd1036
Kalvin <kalvinliang@gmail.com>
2025-02-22 17:31:56 -0500

    modularize my code a bit

--- capture.py
+++ capture.py
@@ -1,11 +1,72 @@
 import argparse
-from scapy.all import sniff, get_if_list, load_layer, DNS, DNSQR, DNSRR, IP
+from scapy.all import * 
 from scapy.layers.tls.extensions import TLS_Ext_ServerName
-from scapy.layers.tls.all import TLS, TLSClientHello
-from scapy.layers.http import HTTPRequest
-import sys
 from datetime import datetime
 import logging
+import sys
+
+SNI_HOST_NAME = 0x00
+logging.basicConfig(level=logging.WARNING)
+
+
+class PacketProcessor:
+    @staticmethod
+    def process(pkt):
+        if not pkt.haslayer(IP):
+            return
+        
+        meta = {
+            "time": datetime.fromtimestamp(pkt.time).strftime("%Y-%m-%d %H:%M:%S.%f"),
+            "src": f"{pkt[IP].src}:{pkt.sport}",
+            "dst": f"{pkt[IP].dst}:{pkt.dport}"
+        }
+
+        if http := PacketProcessor._process_http(pkt):
+            print(f"{meta['time']} HTTP {meta['src']} -> {meta['dst']} {http}")
+        elif tls := PacketProcessor._process_tls(pkt):
+            print(f"{meta['time']} TLS {meta['src']} -> {meta['dst']} {tls}")
+            sys.exit(0)
+        elif dns := PacketProcessor._process_dns(pkt):
+            print(f"{meta['time']} DNS {meta['src']} -> {meta['dst']} {dns}")
+        else:
+            pkt.summary()
+
+    def _process_http(pkt):
+        if not pkt.haslayer(HTTPRequest):
+            return None
+
+        http=pkt[HTTPRequest]
+        try:
+            return f"{http.Host.decode()} {http.Method.decode()} {http.Path.decode()}"
+        except Exception as e:
+            logging.warning(f"Unknown HTTP Error: {e}")
+            return None
+
+    def _process_tls(pkt):
+        client_hello=pkt.getlayer(TLSClientHello)
+        if not client_hello:
+            return None
+        if sni := client_hello.getlayer(TLS_Ext_ServerName):
+            for name in sni.servernames:
+                if name.nametype == SNI_HOST_NAME:
+                    try:
+                        return name.servername.decode('utf-8', errors='ignore')
+                    except Exception as e:
+                        logging.warning(f"Unknown TLS Error: {e}")
+                        return None
+        return None
+
+    def _process_dns(pkt):
+        dnsqr = pkt.getlayer(DNSQR)
+        if dnsqr and dnsqr.qtype == 1:
+            try:
+                return dnsqr.qname.decode('utf-8').rstrip('.')
+            except UnicodeDecodeError:
+                return dnsqr.qname.decode('latin-1').rstrip('.')
+            except Exception as e:
+                logging.warning(f"Unknown DNS Error: {e}")
+                return None
+        return None
 
 class Config():
     
@@ -41,51 +102,49 @@ class Config():
         return f"interface: {self.interface}, tracefile: {self.tracefile}"
 
 
-
-def process_packet(pkt):
-    
-    if IP in pkt:
-        ip_src=pkt[IP].src
-        ip_dst=pkt[IP].dst
-
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    current_time = datetime.now().strftime("%H:%M:%S:%f")
-    if pkt.haslayer(HTTPRequest):
-        http_request = pkt[HTTPRequest]
-        print(f"{current_date} {current_time} HTTP {ip_src} -> {ip_dst} {http_request.Host.decode()} {http_request.Method.decode()} {http_request.Path.decode()}")
-    elif pkt.haslayer(TLS) and pkt.haslayer(TLSClientHello):
-        tls_client_hello = pkt[TLSClientHello]
-        if hasattr(tls_client_hello, 'ext'):
-            for ext in tls_client_hello.ext:
-                # 0x00 here represent sni
-                if ext.type == 0x00:
-                    servernames = ext.servernames
-                    for sn in servernames:
-                        # 0x00 here represent host
-                        if sn.nametype == 0x00:
-                            servername = sn.servername
-                            try:
-                                sni = servername.decode('utf-8')
-                                print(f"{current_date} {current_time} TLS {ip_src} -> {ip_dst} {sni}")
-                            except UnicodeDecodeError:
-                                print("Failed to decode SNI extension data.")
-                            break
-                    else:
-                        print("Does not contain host data")
-                    break
-            else:
-                print(f"{current_date} {current_time} TLS {pkt.src} -> {pkt.dst} NO_SERVER_NAME")
-
-        else:
-            print("No extensions in TLS Client Hello")
-
-    # qtype==1 here refer to A(address) which mean IPv4
-    elif pkt.haslayer(DNS) and pkt.haslayer(DNSQR) and pkt[DNSQR].qtype == 1: 
-        qname = pkt.getlayer(DNSQR).qname.decode()
-        print(f"{current_date} {current_time} DNS {ip_src} -> {ip_dst} {qname} ")
-    else:
-        # print(pkt.summary())
-        print("Not intersting")
+#def process_packet(pkt):
+#    
+#    if IP in pkt:
+#        ip_src=pkt[IP].src
+#        ip_dst=pkt[IP].dst
+#
+#    timestamp = datetime.fromtimestamp(pkt.time)
+#    if pkt.haslayer(HTTPRequest):
+#        http_request = pkt[HTTPRequest]
+#        print(f"{current_date} {current_time} HTTP {ip_src} -> {ip_dst} {http_request.Host.decode()} {http_request.Method.decode()} {http_request.Path.decode()}")
+#    elif pkt.haslayer(TLS) and pkt.haslayer(TLSClientHello):
+#        tls_client_hello = pkt[TLSClientHello]
+#        if hasattr(tls_client_hello, 'ext'):
+#            for ext in tls_client_hello.ext:
+#                # 0x00 here represent sni
+#                if ext.type == 0x00:
+#                    servernames = ext.servernames
+#                    for sn in servernames:
+#                        # 0x00 here represent host
+#                        if sn.nametype == 0x00:
+#                            servername = sn.servername
+#                            try:
+#                                sni = servername.decode('utf-8')
+#                                print(f"{current_date} {current_time} TLS {ip_src} -> {ip_dst} {sni}")
+#                            except UnicodeDecodeError:
+#                                print("Failed to decode SNI extension data.")
+#                            break
+#                    else:
+#                        print("Does not contain host data")
+#                    break
+#            else:
+#                print(f"{current_date} {current_time} TLS {pkt.src} -> {pkt.dst} NO_SERVER_NAME")
+#
+#        else:
+#            print("No extensions in TLS Client Hello")
+#
+#    # qtype==1 here refer to A(address) which mean IPv4
+#    elif pkt.haslayer(DNS) and pkt.haslayer(DNSQR) and pkt[DNSQR].qtype == 1: 
+#        qname = pkt.getlayer(DNSQR).qname.decode()
+#        print(f"{current_date} {current_time} DNS {ip_src} -> {ip_dst} {qname} ")
+#    else:
+#        # print(pkt.summary())
+#        print("Not intersting")
 
     
 
@@ -103,7 +162,7 @@ if __name__ == "__main__":
         print("Confgi build successfully:",config)
         # 1. tracefile is basically a pcap file we pass in to sniff()
         #       e.g. sniff(offline="trace.pcap", prn=callback)
-        sniff(iface=config.interface, offline=config.tracefile, filter=config.expression, prn=process_packet, count = 100)
+        sniff(iface=config.interface, offline=config.tracefile, filter=config.expression, prn=PacketProcessor.process, count = 100)
     except Exception as e:
         logging.error(f"An error occured:{e}")
         sys.exit(1)
